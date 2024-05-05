e1c0cf540ada3597c0f9a5cab6ca1acbfba30b8c
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-05-05 11:21:12 -0300

    Recursao

---
+++ Recursao/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int potencia(int base, int ep){
+
+    if (ep==1){
+        return base;
+    }
+    return base *potencia(base,ep-1);
+}
+
+int main()
+{
+    int v = potencia (2,4); // 16
+    printf("%d",v);
+    return 0;
+}
