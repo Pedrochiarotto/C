850dc4aeda6ff21bd2b0d8573ac05c98c81aa5a9
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-05-06 22:06:21 -0300

    Inserção de dados em listas no final

---
+++ Estrutura de dados/ListaInsercaoFim/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ponto {
+    float x;
+    float y;
+    struct ponto *prox;
+}ponto;
+
+// Ponteiro que aponta para o primeiro elemento da lista
+ponto *listaPontos; 
+
+//Função para adicionar elementos a lista no inicio
+void addIni(float x, float y){
+
+    ponto *p =(ponto*) malloc(sizeof(ponto));
+    p->x=x;
+    p->y=y;
+    p->prox = listaPontos;
+    listaPontos = p;
+}
+//Função para adicionar elementos a lista no final
+void addFim(){
+    ponto *p =(ponto*) malloc(sizeof(ponto));
+    p->x=x;
+    p->y=y;
+    p->prox = NULL; //Será o ultimo elemento da lista
+
+    //Verificar se o primeiro elemento é NULL(Lista vazia)
+
+    if (listaPontos == NULL)
+    {
+        listaPontos = p;
+    }else {
+        ponto *aux = listaPontos;
+        while (aux->prox !=NULL)
+        {
+            aux=aux->prox;
+        }
+        aux->prox=p;
+    }
+    
+}
+
+//Função para imprimir a lista
+void imprime(){
+    ponto *auxLista = listaPontos; // Aponta para a primeira posição da lista
+    while(auxLista != NULL){
+        printf("\nPonto(%.1f,%.1f)",auxLista->x, auxLista->y);
+        auxLista =auxLista ->prox;//A variável auxiliar irá receber a proxima posicao
+    }
+}
+
+int main()
+{
+    addIni(1,5);
+    addIni(2,7);
+    addIni(5,3);
+
+    printf("%.0f", listaPontos->prox->x);
+
+    imprime();
+    // criação de encadeamento
+    // p->prox = p1;
+    // p1->prox = p2;
+    // p2->prox = NULL
+}
