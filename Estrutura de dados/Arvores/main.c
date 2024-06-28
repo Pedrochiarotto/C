a90c6046ec658537e969a2a1b761652380dddb3e
Pedro Henrique Chiarotto <131906702+Pedrochiarotto@users.noreply.github.com>
2024-06-28 11:57:09 -0300

    remoção de arvores

--- Estrutura de dados/Arvores/main.c
+++ Estrutura de dados/Arvores/main.c
@@ -46,6 +46,78 @@ void add(nodo *n, int valor){
     }
     
 }
+void rem(nodo *n, int valor){
+    nodo *filho = n;
+    nodo *pai;
+    
+    do{
+        pai = filho;
+        if(valor < filho->valor){
+            filho = filho->esq;
+        }else if(valor > filho->valor){
+            filho = filho->dir;
+
+        }
+    }while(filho != NULL && filho->valor != valor);
+
+    if (filho != NULL)
+    {
+        if(filho->esq == NULL && filho->dir == NULL){//caso 1: nodo nao tem filhos.
+            if (pai->esq == filho)
+            {
+                pai->esq = NULL;
+            }
+            if (pai->dir == filho)
+            {
+                pai->dir = NULL;
+            }
+        }
+        if (filho->esq != NULL && filho->dir == NULL)//Caso 2: nodo filho tem um filho a esquerda.
+        {
+            if (pai->esq == filho)
+            {
+                pai->esq = filho->esq;
+            }
+            if (pai->dir == filho)
+            {
+                pai->dir = filho->esq;
+            }
+            
+            
+        }
+        if (filho->dir != NULL && filho->esq == NULL)//Caso 2: nodo filho tem um filho a direita.
+        {
+            if (pai->esq == filho)
+            {
+                pai->esq = filho->dir;
+            }
+            if (pai->dir == filho)
+            {
+                pai->dir = filho->dir;
+            }
+        }
+        //Caso 3: Nodo filho tem um filho a direita e a esquerda
+       if(filho->esq != NULL && filho->dir != NULL) {
+        if (filho->esq->dir == NULL)
+        {
+            filho->valor = filho->esq->valor;
+        }else{
+            nodo *p = filho->esq;
+            nodo *aux = p;
+            while (p->dir !=NULL)
+            {
+                aux= p;
+                p = p->dir;
+
+            }
+            aux->dir = NULL;
+            filho->valor = p->valor;
+        }
+        
+       }
+    }
+    
+} 
 
 int main()
 {
@@ -63,7 +135,11 @@ int main()
     nodo2->dir = nodo4;*/
 
     //Passar o nodo raiz(inicial) para exibir a arvore toda
+   
+    rem(root, 2);
+
     imprimir(root) ;
+ 
 
     return 0; 
     
